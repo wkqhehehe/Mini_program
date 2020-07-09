568240d2e772d005db150637cec3b3e05a1d26f1
wkq <706752878@qq.com>
2020-07-09 11:04:19 +0800

    第三次提交
    less样式无法应用

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,32 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//Page Object
+import {
+  request
+} from "../../request/index.js"
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    swiperList: [],
+    cateList:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+  //options(Object)
+  onLoad: function (options) {
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList:result.data.message
+    //     })
+    //   }
+    // })
+
+    request({
+        url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      .then(result => {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          swiperList:result.data.message
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      });
+     
   }
-})
+
+});
